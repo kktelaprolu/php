c6eac655cfd72f21e1767308783a7a866ce2adc9
Krishna Kanth Telaprolu <36309708+kktelaprolu@users.noreply.github.com>
2024-08-03 08:37:31 +0000

    python practise

--- python/file_open.py
+++ python/file_open.py
@@ -1,11 +1,20 @@
-file = open('example.txt', 'r')
+import sys
+import os
+from os import system
+
+file = open('python/123.txt','r')
 content = file.read()
+print(content)
 file.close()
 
-'''
 print("------------")
 
-with open('example.txt', 'r'):
-  content = file.read()
+with open('python/123.txt','r') as file:
+    content = file.read()
+    print(content)
 
-'''
+
+with open('python/example.txt','w') as file:
+    file.write("How are you?")
+
+system("cat ./python/example.txt")
